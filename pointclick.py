9d2983a19c74a2bd8a9d73e755306c8d4681ec70
HamsterMan69 <66910024+HamsterMan69@users.noreply.github.com>
2023-03-08 11:36:07 +0200

    Update pointclick.py

--- pointclick.py
+++ pointclick.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 from sprites import *
 
 
@@ -12,7 +13,16 @@ class Point_Click():
 
     def areas(self):
         self.all = pygame.sprite.Group()
-        self.uksed = Voco_uksed(1280, 768)
+        self.uksed = Voco_uks(0, 0)
+        self.fuajee = Voco_fuajee(0, 0)
+        self.korridor = Voco_korridor(0, 0)
+        self.trepp = Voco_trepp(0, 0)
+        self.trepp2 = Voco_trepp2(0, 0)
+        self.trep3 = Voco_trep3(0, 0)
+        self.korrus3 = Voco_3korrus(0, 0)
+        self.klass = Voco_klass(0, 0)
+        self.joonas = Voco_joonas(0, 0)
+        self.margus = Voco_margus(0, 0)
         self.all.add(self.uksed)
 
     def run(self):
@@ -20,22 +30,24 @@ class Point_Click():
         while self.running:
             mx, my = pygame.mouse.get_pos()
     
-        self.all.draw(self.screen)
-        self.uksed.run()
-        
-   #     if self.test.rect.collidepoint((mx, my)):
-    #        if click:
-     #           self.screen.blit(pygame.image.load(f'{i}asendus fuajee.png'), (0, 0))
+            self.all.draw(self.screen)
+            #self.uksed.run()
+            
+            if self.uksed.rect.collidepoint((mx, my)):
+                if click:
+                    self.all.add(self.margus)
 
-        click = False       
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        click = True
-                    if event.button == 0:
-                        click = True
-        pygame.display.update()
+            click = False       
+            for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        self.running = False
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if event.button == 1:
+                            click = True
+                        if event.button == 0:
+                            click = True
+            pygame.display.update()
+            
+            
         
         
