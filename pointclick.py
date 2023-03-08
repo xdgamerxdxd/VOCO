0fc3eb010e6c0bc5e93fa6e2c6ccadc9a50a9acc
HamsterMan69 <66910024+HamsterMan69@users.noreply.github.com>
2023-03-08 09:30:00 +0200

    pointclick

--- pointclick.py
+++ pointclick.py
@@ -6,18 +6,25 @@ class Point_Click():
     def __init__(self, screen, state):
         self.screen = screen
         self.running = state
+        
         pygame.display.set_caption("Point & Click")
+
+    def areas(self):
+        self.all = pygame.sprite.Group()
+        self.uksed = Voco_uksed(1280, 768)
+        self.all.add(self.uksed)
 
     def run(self):
         click = False
         while self.running:
-            self.screen.blit(pygame.image.load(f'{i}voco_uksed.png'), (0, 0))
             mx, my = pygame.mouse.get_pos()
     
-
-        if self.test.rect.collidepoint((mx, my)):
-            if click:
-                self.screen.blit(pygame.image.load(f'{i}asendus fuajee.png'), (0, 0))
+        self.all.draw(self.screen)
+        self.uksed.run()
+        
+   #     if self.test.rect.collidepoint((mx, my)):
+    #        if click:
+     #           self.screen.blit(pygame.image.load(f'{i}asendus fuajee.png'), (0, 0))
 
         click = False       
         for event in pygame.event.get():
